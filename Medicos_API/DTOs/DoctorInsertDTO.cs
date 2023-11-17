3081b6e152e9f348250f45d2b5706409e5c389d0
Caio Diego <caiodsj96@gmail.com>
2023-11-17 17:28:55 -0300

    fix: add cpf doctor model

--- Medicos_API/DTOs/DoctorInsertDTO.cs
+++ Medicos_API/DTOs/DoctorInsertDTO.cs
@@ -4,11 +4,12 @@ namespace Medicos_API.DTOs
 {
     public class DoctorInsertDTO
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
+        public string CPF { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Specialty { get; set; }
-        public string CRM { get; set; }
+        public required string CRM { get; set; }
         public bool AcceptSample { get; set; }
         public AdressInsertDTO Adress { get; set; }
     }
